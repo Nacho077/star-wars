1c2b2b78f444fb6b5932ffe51566640fbad8206d
Ignacio <ignaciogimenez70@gmail.com>
2024-07-15 18:16:36 -0300

    debug

--- client/src/configurations/axios.js
+++ client/src/configurations/axios.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+console.log(import.meta.env)
+console.log(import.meta.env.VITE_API_BASE_URL)
 axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL
 
 export const setAuthorization = token => {
